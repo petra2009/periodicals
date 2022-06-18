3da7d7dd4a534740336d9fb63ba2976da8086a91
admin <ovchinnikov_petr@mail.ru>
2022-06-18 18:52:26 +0300

    heroku

--- src/main/java/com/example/periodicals/web/controllers/HomeController.java
+++ src/main/java/com/example/periodicals/web/controllers/HomeController.java
@@ -11,12 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String getHomePage() {
         return "home";
     }
-
-
 
 
 }
